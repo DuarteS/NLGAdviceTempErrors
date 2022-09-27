5359d2d0c149322d981776ed8c178b9e7ad49b08
Duarte <somsenduarte@gmail.com>
2022-09-27 11:23:35 +0200

    Changed print

--- TemperatureTextAdvice/main.py
+++ TemperatureTextAdvice/main.py
@@ -2,4 +2,5 @@ from CreateText import create_Text
 from Reasons import getValues
 from Results import gen_results
 
-print(create_Text(gen_results(getValues())))
+for result in create_Text(gen_results(getValues())):
+    print(result[0], ' - ', result[1], result[2], '  #  ', result[3], '  #  ', result[4])
