d3c5c1823d76b7970fd221eaec197b5d1e79c1cf
Duarte Somsen <ds@letsgrow.com>
2022-10-18 09:23:14 +0200

    Update

--- TemperatureTextAdvice/TempShock/GetData.py
+++ TemperatureTextAdvice/TempShock/GetData.py
@@ -11,7 +11,7 @@ TEMP_FILE = "../TempShock/TotalData.csv"
 SHOCK_TYPE = "kastemperatuur"  # "absoluut vochtgehalte meetbox onder" / "kastemperatuur"
 
 
-def _get_temp_data(temp_csv):
+def _get_data(data_csv):
     headers = ['buitentemperatuur', 'energiedoek', 'absoluut vochtgehalte kaslucht boven doek', 'kastemperatuur',
                'absoluut vochtgehalte meetbox onder', 'kastemperatuur meetbox onder', 'kastemperatuur boven doek']
     try:
@@ -20,45 +20,41 @@ def _get_temp_data(temp_csv):
     except ValueError:
         print("not found")
 
-    df_temperature = pd.read_csv(temp_csv, sep=',').drop(headers, axis=1).dropna()
+    df_data = pd.read_csv(data_csv, sep=',').drop(headers, axis=1).dropna()
 
-    df_temperature['local_time'] = pd.to_datetime(df_temperature['local_time'], format='%d/%m/%Y %H:%M')
-    df_temperature['local_time'] = pd.to_datetime(df_temperature['local_time'].astype(str), format='%Y/%m/%d %H:%M:%S')
+    df_data['local_time'] = pd.to_datetime(df_data['local_time'], format='%d/%m/%Y %H:%M')
+    df_data['local_time'] = pd.to_datetime(df_data['local_time'].astype(str), format='%Y/%m/%d %H:%M:%S')
 
-    df_temperature[SHOCK_TYPE] = df_temperature[SHOCK_TYPE].astype(float)
-    return df_temperature
+    df_data[SHOCK_TYPE] = df_data[SHOCK_TYPE].astype(float)
+    return df_data
 
 
-def _find_shock_connections(df_temp):
+def _find_shock_connections(df_data):
     dif_p_5m = (max_var_hour / shock_time) * 5
-    df_temp_shock = pd.DataFrame(columns=['start_index', 'end_index'])
+    df_data_shock = pd.DataFrame(columns=['start_index', 'end_index'])
 
-    for curr_row in df_temp.iterrows():
+    for curr_row in df_data.iterrows():
         test_row = curr_row
         break
 
-    for curr_row in df_temp.iterrows():
+    for curr_row in df_data.iterrows():
         test_temp = test_row[1][SHOCK_TYPE]
         curr_temp = curr_row[1][SHOCK_TYPE]
-        temp_increase = 0
 
-        if curr_temp >= test_temp:
-            temp_increase = curr_temp - test_temp
-        elif test_temp > curr_temp:
-            temp_increase = test_temp - curr_temp
+        temp_increase = abs(test_temp-curr_temp)
 
         if temp_increase >= dif_p_5m:
             EndIndex = curr_row[0]
-            StartIndex = df_temp[
-                df_temp['local_time'] == curr_row[1].local_time - timedelta(hours=00, minutes=shock_time)].index.values
+            StartIndex = df_data[
+                df_data['local_time'] == curr_row[1].local_time - timedelta(hours=00, minutes=shock_time)].index.values
             time = shock_time
             while not StartIndex:
                 time = time - 5
-                StartIndex = df_temp[df_temp['local_time'] == curr_row[1].local_time -
+                StartIndex = df_data[df_data['local_time'] == curr_row[1].local_time -
                                      timedelta(hours=00, minutes=time)].index.values
             StartIndex = StartIndex[0]
 
-            time_group = df_temp.iloc[StartIndex: EndIndex]
+            time_group = df_data.iloc[StartIndex: EndIndex]
 
             max_var = time_group[SHOCK_TYPE].max()
             min_var = time_group[SHOCK_TYPE].min()
@@ -67,31 +63,28 @@ def _find_shock_connections(df_temp):
 
             if max_var - min_var >= max_var_hour:
 
-                for temp_row in df_temp_shock.iterrows():
+                for temp_row in df_data_shock.iterrows():
                     test_start_time = temp_row[1].start_index
                     test_end_time = temp_row[1].end_index
 
                     if test_start_time < StartIndex < test_end_time:
-                        df_temp_shock.loc[temp_row[0], 'end_index'] = EndIndex
-
+                        df_data_shock.loc[temp_row[0], 'end_index'] = EndIndex
                         added = True
-                        break
 
                 if not added:
-                    df_temp_shock.loc[df_temp_shock.shape[0]] = [StartIndex, EndIndex]
+                    df_data_shock.loc[df_data_shock.shape[0]] = [StartIndex, EndIndex]
 
         test_row = curr_row
 
-    print(df_temp_shock.head(20))
-    return df_temp_shock
+    return df_data_shock
 
 
 def _get_shock_plot_data(df_temp, df_shock_connected):
     df_shock_plot_data = pd.DataFrame(columns=['start_time', 'end_time', 'max_var', 'min_var'])
+
     for curr_row in df_shock_connected.iterrows():
         time_group = df_temp.iloc[curr_row[1].start_index: curr_row[1].end_index]
 
-        print(time_group)
         start_time = time_group['local_time'].min()
         end_time = time_group['local_time'].max()
 
@@ -123,7 +116,51 @@ def plot_online_shock(df_data, df_shock):
     subfig.show()
 
 
-df = _get_temp_data(TEMP_FILE)
+def _get_shock_analytics_data(df_temp, df_shock_connected):
+    for curr_row in df_shock_connected.iterrows():
+        time_group = df_temp.iloc[curr_row[1].start_index: curr_row[1].end_index]
+        max_var = time_group[SHOCK_TYPE].max()
+        min_var = time_group[SHOCK_TYPE].min()
+        start_time = time_group['local_time'].min()
+        end_time = time_group['local_time'].max()
+
+        duration_hours = (end_time-start_time) / pd.timedelta(hours=1)
+        duration_minutes = (end_time - start_time) / pd.timedelta(minutes=1)
+
+        min_max_differance = max_var - min_var
+
+        avg_change_p5m = (min_max_differance / duration_minutes) * 5
+
+        if min_var == 0:
+            sensors_down = True
+        else:
+            sensors_down = False
+
+        overall_differance = 0
+        peaks = 0
+        curr_direction = "none"
+
+        cal_temp = time_group[SHOCK_TYPE].iat[0]
+        for timegroup_row in time_group.iterrows():
+            row_temp = timegroup_row[1][SHOCK_TYPE]
+
+            overall_differance = overall_differance + abs(cal_temp-row_temp)
+
+            if cal_temp-0.5 > row_temp:
+                curr_direction = "down"
+            elif cal_temp+0.5 < row_temp and (curr_direction == "down" or curr_direction == "none"):
+                peaks = peaks + 1
+                curr_direction = "up"
+
+            # Add short time raise
+
+            cal_temp = row_temp
+
+        print(peaks)
+
+
+df = _get_data(TEMP_FILE)
 shock_connections = _find_shock_connections(df)
-shock_plot_data = _get_shock_plot_data(df, shock_connections)
-plot_online_shock(df, shock_plot_data)
+_get_shock_analytics_data(df, shock_connections)
+# shock_plot_data = _get_shock_plot_data(df, shock_connections)
+# plot_online_shock(df, shock_plot_data)
